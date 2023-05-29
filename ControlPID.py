4fab92ed3d1c970a03c5b20f84dfef388362e460
MasterKiri13 <leoneleduardomadrid@hotmail.com>
2023-05-29 16:37:49 -0600

    changed things related to the raspberry pi code

--- ControlPID.py
+++ ControlPID.py
@@ -3,6 +3,7 @@ import board
 import adafruit_bno055
 import numpy as np
 import math
+from gpiozero import Motor
 from simple_pid import PID
 import RPi.GPIO as GPIO
 
@@ -10,32 +11,39 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 
 # GPIO Pins Motor 1 
-motor1_in1 = 24
-motor1_in2 = 23
-motor1_en = 25
-GPIO.setup(motor1_in1, GPIO.OUT)
-GPIO.setup(motor1_in2, GPIO.OUT)
-GPIO.setup(motor1_en, GPIO.OUT)
-motor1_pwm = GPIO.PWM(motor1_en, 100)
-motor1_pwm.start(0)
+enable_hover = 25
+GPIO.setup(enable_hover, GPIO.OUT)
 
-# GPIO Pins Motor 2 
-motor2_in1 = 27
-motor2_in2 = 22
-motor2_en = 17
-GPIO.setup(motor2_in1, GPIO.OUT)
-GPIO.setup(motor2_in2, GPIO.OUT)
-GPIO.setup(motor2_en, GPIO.OUT)
-motor2_pwm = GPIO.PWM(motor2_en, 100)
-motor2_pwm.start(0)
+# GPIO2 I2C1 SDA
+# GPIO3 I2C1 SCL
 
-i2c = board.I2C()  
-sensor1 = adafruit_bno055.BNO055_I2C(i2c, 0x28)
-sensor2 = adafruit_bno055.BNO055_I2C(i2c, 0x29)
+i2c = board.I2C()
+
+sensor1 = adafruit_bno055.BNO055_I2C(i2c,0x28) #izquierdo
+sensor2 = adafruit_bno055.BNO055_I2C(i2c,0x29) #derecho
+
+# motor izquierdo GPIO pins 23(red) 24(black) 25(enable)
+# motor derecho GPIO pins 17(red) 27(black) 22(enable)
+
+izq = Motor(23,24,25)
+der = Motor(17,27,22)
+
+x1 = 0.0
+x2 = 0.0
+
+#PID weights and constants
+Kp = 1
+Ki = 0.1
+Kd = 0.05
+
+setpoint = 0
+output_limits=(-50, 50)
+
+limit = 1.5
 
 # Controladores pid por motor. Los output limits son la velocidad de cada actuador
-pid1 = PID(Kp=1, Ki=0.1, Kd=0.05, setpoint=0, output_limits=(-50, 50), auto_mode=True)  
-pid2 = PID(Kp=1, Ki=0.1, Kd=0.05, setpoint=0, output_limits=(-50, 50), auto_mode=True) 
+pid1 = PID(Kp, Ki, Kd, setpoint, output_limits, auto_mode=True)  
+pid2 = PID(Kp, Ki, Kd, setpoint, output_limits, auto_mode=True) 
 
 def pitch(quat, accel):
     if(quat[0] != None and quat[1] != None and quat[2] != None and quat[3] != None):
@@ -56,31 +64,27 @@ while True:
     control2 = pid2(quat2, accel2)
 
     #Implementacion del control 
-    if control1 >= 0:
-        GPIO.output(motor1_in1, GPIO.HIGH)
-        GPIO.output(motor1_in2, GPIO.LOW)
-        motor1_pwm.ChangeDutyCycle(min(abs(control1), 100))
+    if control1 >= limit:
+        izq.forward(1)
+    elif control1 <= -limit:
+        izq.backward(1)
     else:
-        GPIO.output(motor1_in1, GPIO.LOW)
-        GPIO.output(motor1_in2, GPIO.HIGH)
-        motor1_pwm.ChangeDutyCycle(min(abs(control1), 100))
+        izq.stop()
 
-    if control2 >= 0:
-        GPIO.output(motor2_in1, GPIO.HIGH)
-        GPIO.output(motor2_in2, GPIO.LOW)
-        motor2_pwm.ChangeDutyCycle(min(abs(control2), 100))
+    if control2 >= limit:
+        der.forward(1)
+    elif control2 <= -limit:
+        der.backward(1)
     else:
-        GPIO.output(motor2_in1, GPIO.LOW)
-        GPIO.output(motor2_in2, GPIO.HIGH)
-        motor2_pwm.ChangeDutyCycle(min(abs(control2), 100))
+        der.stop()
 
     print("Sensor 1")
-    print("Pitch: ", quat1)
-    print("Acceleration: ", accel1)
-    print("Control: ", control1)
+    print("Pitch: {}".format( quat1))
+    print("Acceleration: {}".format(accel1))
+    print("Control: {}\n".format(control1))
     print("Sensor 2")
-    print("Pitch: ", quat2)
-    print("Acceleration: ", accel2)
-    print("Control: ", control2)
+    print("Pitch: {}".format(quat2))
+    print("Acceleration: {}".format(accel2))
+    print("Control: {}\n".format(control2))
 
-    time.sleep(0.1)
+    time.sleep(0.05)
--- control.py
+++ control.py
@@ -2,14 +2,19 @@ from gpiozero import Motor
 import adafruit_bno055
 import board
 import time
+import math
 import numpy as np
 
-def pitch(quat):
-    #create pitch angle from quaternion
-    pitch = np.arcsin(2*quat.w()*quat.y()-quat.x()*quat.z())
 
-    #convert Radians to degrees
-    return 57.2958 * pitch
+def pitch(quat, accel):
+    if(quat[0] != None and quat[1] != None and quat[2] != None and quat[3] != None):
+        pitch = np.arcsin(2 * quat[0] * quat[2] - quat[1] * quat[3])
+    else:
+        pitch = 0.0
+    if math.isnan(pitch) or math.isnan(accel): 
+        pitch = 0.0
+        accel = 0.0
+    return 57.2958 * pitch, accel
 
 def main():
     # GPIO2 I2C1 SDA
@@ -17,8 +22,8 @@ def main():
 
     i2c = board.I2C()
 
-    sensor1 = adafruit_bno055.BNO055_I2C(i2c) #izquierdo
-    sensor2 = adafruit_bno055.BNO055_I2C(i2c) #derecho
+    sensor1 = adafruit_bno055.BNO055_I2C(i2c,0x28) #izquierdo
+    sensor2 = adafruit_bno055.BNO055_I2C(i2c,0x29) #derecho
 
     # motor izquierdo GPIO pins 23(red) 24(black) 25(enable)
     # motor derecho GPIO pins 17(red) 27(black) 22(enable)
@@ -29,14 +34,12 @@ def main():
     x1 = 0.0
     x2 = 0.0
     while(True):
-        print("sensor 1:")
-        print(sensor1.euler[0])
+        x1, acc1 = pitch(sensor1.quaternion, sensor1.acceleration[2])
+        x2, acc2 =pitch(sensor2.quaternion, sensor2.acceleration[2])
 
-        print("sensor 2: ")
-        print(sensor2.euler[0])
+        print("sensor 1: {}".format(x1))
+        print("sensor 2: {}".format(x2))
     
-        x1 = sensor1.euler[0]
-        x2 = sensor2.euler[0]
     
         if(x1 != None and x2 != None):
             if(x1 < 1.5 and x1 > 358.5):
